b15023263f3f23cbc5138699e5556d2fd48d634d
Amar Oklopcic <amar_oklopcic@live.com>
2024-04-14 22:04:34 -0500

    temporarily disable fonts generation

--- lua/autorun/client/fonts.lua
+++ lua/autorun/client/fonts.lua
@@ -1,38 +1,46 @@
--- This table is just going to hold all of the generated fonts for later use.
-fontRatioChangeTable = fontRatioChangeTable or {}
+include("autorun/client/cvars.lua")
 
--- Doing this just so we could remake fonts and see ratio effects live. Kinda messy, sorry :/
-hook.Add("mCompass_loadFonts", "mCompass_loadFonts_addon", function()
-    local h = compass_style.height
-    local r = compass_style.ratio
-    local ms = ScrH() * (compass_style.maxMarkerSize / 45)
-    if r != mCompass_oldFontRatio then
-        for k, v in pairs(fontRatioChangeTable) do
-            if "exo_compass_Numbers_" .. r == v.numberName then
-                mCompass_oldFontRatio = r
-                return v
-            end
-        end
-        surface.CreateFont("exo_compass_Numbers_"..r, {
-            font = "Exo",
-            size = math.Round((ScrH() * h) / r),
-            antialias = true
-        })
-        surface.CreateFont("exo_compass_Distance-Display-Numbers_"..r, {
-            font = "Exo",
-            size = (ScrH() * (h / r)) * compass_style.maxMarkerSize,
-            antialias = true
-        })
-        surface.CreateFont("exo_compass_Letters", {
-            font = "Exo",
-            size = ScrH() * h,
-            antialias = true
-        })
-        local t = {
-            ratio = r,
-            numberName = "exo_compass_Numbers_"..r
-        }
-        table.insert(fontRatioChangeTable, t)
-        mCompass_oldFontRatio = r
-    end
+
+-- This table is just going to hold all of the generated fonts for later use.
+-- cached fonts for main headings ("N", "W", "E", "S"), subheadings (e.g. "NE", "SW", etc.), and number headings
+main_heading_fonts = main_heading_fonts or {}
+subheading_fonts = subheading_fonts or {}
+number_heading_fonts = number_heading_fonts or {}
+
+-- TODO: refactor this
+-- hook that can be called to regenerate fonts as settings are changed in real-time
+-- fonts are only created once, then cached for later use
+hook.Add("mcompass_generate_fonts", "mcompass_generate_fonts_addon", function()
+    print("regenerating fonts...")
+    -- local h = compass_style.height
+    -- local r = compass_style.ratio
+    -- if r != mCompass_oldFontRatio then
+    --     for k, v in pairs(fontRatioChangeTable) do
+    --         if "exo_compass_Numbers_" .. r == v.numberName then
+    --             mCompass_oldFontRatio = r
+    --             return v
+    --         end
+    --     end
+    --     surface.CreateFont("exo_compass_Numbers_" .. r, {
+    --         font = "Exo",
+    --         size = math.Round((ScrH() * h) / r),
+    --         antialias = true
+    --     })
+    --     surface.CreateFont("exo_compass_Distance-Display-Numbers_" .. r, {
+    --         font = "Exo",
+    --         size = (ScrH() * (h / r)) * compass_style.maxMarkerSize,
+    --         antialias = true
+    --     })
+    --     surface.CreateFont("exo_compass_Letters", {
+    --         font = "Exo",
+    --         size = ScrH() * h,
+    --         antialias = true
+    --     })
+    --     local t = {
+    --         ratio = r,
+    --         numberName = "exo_compass_Numbers_" .. r
+    --     }
+    --     table.insert(fontRatioChangeTable, t)
+    --     mCompass_oldFontRatio = r
+    -- end
 end)
